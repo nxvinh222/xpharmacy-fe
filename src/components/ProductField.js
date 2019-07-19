983d7f9210d7305b8542dddc0776f0f7aec9c6b3
unknown <nxvinh222@gmail.com>
2019-07-19 21:04:28 +0700

    update

--- src/components/ProductField.js
+++ src/components/ProductField.js
@@ -5,6 +5,7 @@ class ProductField extends Component {
     
     render() {
         const allproduct = this.props.products.map(item => <Product product={item}/>)
+        console.log(this.props.products)
         return (
             <div className="row mb-5">
                 {allproduct}
