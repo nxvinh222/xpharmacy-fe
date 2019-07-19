71ee534b15996fd8693779b0f791bd04c511dc50
unknown <nxvinh222@gmail.com>
2019-07-19 21:45:49 +0700

    add to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class product extends Component {
+
+    _addToCart = (event) => {
+        var cart;
+        if (localStorage.getItem('cart')==null)
+            cart = []
+        else
+            cart = JSON.parse(localStorage.getItem('cart'));
+        cart.push(this.props.product._id);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        event.preventDefault();
+    }
     render() {
         return (
             <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
@@ -12,7 +23,9 @@ class product extends Component {
                         <h3><a href="shop-single.html">{this.props.product.name}</a></h3>
                         <p className="mb-0">{this.props.product.info}s</p>
                         <p className="text-primary font-weight-bold">{this.props.product.price}$</p>
-                        <a href="#" class="btn btn-primary">ADD TO CART</a>
+                        <a href="#" 
+                            onClick={this._addToCart} class="btn btn-primary">ADD TO CART
+                        </a>
                     </div>
                 </div>
             </div>
