7b9a4ec9bd5c1debd18a0e24db290cf3e0d1dfe8
divinevinh <nxvinh222@gmail.com>
2019-09-28 21:22:39 +0700

    add remove cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -13,12 +13,29 @@ class CartItem extends Component {
             this.setState({
                 image: data.data.image,
                 price: data.data.price,
-                name: data.data.name
+                name: data.data.name,
+                id: this.props.id
             })
             // console.log(data.data)
             }
         )
     }
+    arrayRemove(arr, value) {
+
+        return arr.filter(function(ele){
+            return ele.id != value;
+        });
+     
+     }
+    Delete = (event) => {
+        event.preventDefault();
+        console.log("ok")
+        var cart = JSON.parse(localStorage.getItem('cart'))
+        console.log(cart[0].id)
+        localStorage.setItem('cart', JSON.stringify(this.arrayRemove(cart, this.props.id)));
+        window.location.href='/cart'
+    }
+
     render() {
         return (
             <div className="row" >
@@ -34,6 +51,10 @@ class CartItem extends Component {
                 <div className="col-md-2">
                     Total = {this.state.price * this.props.quantity}
                 </div>
+                <form onClick={this.Delete} className="FormFields">
+                    <a href="#" className="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Delete</a>
+                </form>
+                
             </div>
             
         );
