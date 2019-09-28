5b5b6d0e8de3de1c0b3b683d26f06e50f23e5ede
unknown <nxvinh222@gmail.com>
2019-09-28 17:47:26 +0700

    pull

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,29 +1,42 @@
 import React, { Component, useState } from 'react';
 import axios from '../axios';
 import { Modal, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 class product extends Component {
     state = {
-        field: this.props.field,
-        show: false
+        field: this.props.field
     }
-
-    showModal = () => {
-        this.setState({ show: true });
-      };
-    
-      hideModal = () => {
-        this.setState({ show: false });
-      };
-
     _addToCart = (event) => {
         var cart;
-        if (localStorage.getItem('cart')==null)
-            cart = []
-        else
+        if (localStorage.getItem('cart')==null){
+                cart = []
+                cart.push({
+                    id: this.props.product._id,
+                    quantity: 1,
+                    price: this.props.product.price,
+                    name: this.props.product.name
+                });
+        }
+        else{
             cart = JSON.parse(localStorage.getItem('cart'));
-        cart.push(this.props.product._id);
+            var flag = false
+            for (let element of cart){
+                if (element.id == this.props.product._id){
+                    element.quantity++;
+                    flag = true;
+                    break;
+                }              
+            }
+            if (flag!=true){
+                cart.push({
+                    id: this.props.product._id,
+                    quantity: 1,
+                    price: this.props.product.price,
+                    name: this.props.product.name
+                });
+            }
+        }
+        
         localStorage.setItem('cart', JSON.stringify(cart));
         event.preventDefault();
     }
@@ -33,43 +46,42 @@ class product extends Component {
             .delete(`/api/v1/products/${this.props.product._id}`)
             .then(alert("Delete success"))
             .catch(err => console.log(err));
-        this.setState({ show: false })
     }
 
     render() {
-        // const Delete = () => {
-        //     const [show, setShow] = useState(false);
+        const Delete = () => {
+            const [show, setShow] = useState(false);
           
-        //     const handleClose = () => setShow(false);
-        //     const handleShow = () => setShow(true);
-        //     const handleDelete = () => {
-        //         this._deleteProd();
-        //         setShow(false);
-        //     }
+            const handleClose = () => setShow(false);
+            const handleShow = () => setShow(true);
+            const handleDelete = () => {
+                this._deleteProd();
+                setShow(false);
+            }
           
-        //     return (
-        //       <>
-        //         <Button variant="primary" onClick={handleShow}>
-        //           Delete
-        //         </Button>
+            return (
+              <>
+                <Button variant="primary" onClick={handleShow}>
+                  Delete
+                </Button>
           
-        //         <Modal show={show} onHide={handleClose}>
-        //           <Modal.Header closeButton>
-        //             <Modal.Title>Verify</Modal.Title>
-        //           </Modal.Header>
-        //           <Modal.Body>Are you sure you wish to delete this item?</Modal.Body>
-        //           <Modal.Footer>
-        //             <Button variant="secondary" onClick={handleClose}>
-        //               No
-        //             </Button>
-        //             <Button variant="primary" onClick={handleDelete}>
-        //               Yes
-        //             </Button>
-        //           </Modal.Footer>
-        //         </Modal>
-        //       </>
-        //     );
-        // }
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Verify</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>Are you sure you wish to delete this item?</Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                      No
+                    </Button>
+                    <Button variant="primary" onClick={handleDelete}>
+                      Yes
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
+            );
+        }
         
         const func = () => {
             let button
@@ -80,46 +92,19 @@ class product extends Component {
                     </a>
                 )
             } else if(this.state.field === "delete") {
-                button = (
-                    <>
-                        <Button variant="primary" onClick={this.showModal}>
-                        Delete
-                        </Button>
-                
-                        <Modal show={this.state.show} onHide={this.hideModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Verify</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>Are you sure you wish to delete this item?</Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={this.hideModal}>
-                            No
-                            </Button>
-                            <Button variant="primary" onClick={this._deleteProd}>
-                            Yes
-                            </Button>
-                        </Modal.Footer>
-                        </Modal>
-                    </>
-                )
+                button = ( <Delete/> )
             }
             return button;
         }
         const hello = () => console.log(this.state.field);
         return (
             <div className="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
-                <div key={this.props.product._id} className="block-4 text-center border">
+                <div className="block-4 text-center border">
                     <figure className="block-4-image">
-                    <Link to={`/products/${this.props.product._id}`}>
-                        <img src={this.props.product.image} alt="Image placeholder" className="img-fluid"/>
-                    </Link>
+                    <a href={`/api/v1/products/${this.props.product._id}`}><img src={this.props.product.image} alt="Image placeholder" className="img-fluid"/></a>
                     </figure>
                     <div className="block-4-text p-4">
-                        <h3>
-                            <Link to={`/products/${this.props.product._id}`}>
-                                {this.props.product.name}
-                            </Link>
-                        </h3>
+                        <h3><a href="shop-single.html">{this.props.product.name}</a></h3>
                         <p className="text-primary font-weight-bold">{this.props.product.price}$</p>
                         {func()}
                     </div>
